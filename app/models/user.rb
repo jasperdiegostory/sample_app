adc1a9c5ec5f936caa47f3aed8b8c36e3440cb30
jasperdiegostory <jasper@viewsy.com>
2012-11-29 10:42:11 +0000

    Factory Girl

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # == Schema Information
 #
 # Table name: users
@@ -9,12 +8,11 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-
-class User < ActiveRecord::Base
+class User < ActiveRecord::Base 
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
-  before_save { |user| user.email = email.downcase }
+  before_save { self.email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -24,8 +22,3 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 end
-=======
-class User < ActiveRecord::Base
-  attr_accessible :email, :name
-end
->>>>>>> modeling-users
