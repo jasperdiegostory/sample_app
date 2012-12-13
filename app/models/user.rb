48ff36162b6166b25b060627b55ae47bee57935b
jasperdiegostory <jasper@viewsy.com>
2012-12-13 14:56:13 +0000

    Remove debug in application layour

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@
 class User < ActiveRecord::Base 
   attr_accessible :name, :email, :password, :password_confirmation 
   #these attributes, and only these attributes, are accessible (can be modified by outside users)
-  has_secure_password
+  has_secure_password #built in. needs model column "password_digest". built in functionality includes raw passwords are viritual (not persisted to database). this also tests that password is the same as password confirmation, and also that the :password entered matches the password-digest column in the database.
 
   before_save { |user| user.email = email.downcase } #before committing to memory, emails are standardized
   before_save :create_remember_token
